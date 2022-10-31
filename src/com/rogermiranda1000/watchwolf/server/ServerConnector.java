d7a64bced4d89c31abf1b6f590090e8c6d729f35
roger.miranda <roger.miranda@students.salle.url.edu>
2022-10-31 16:20:53 +0100

    Fix: byte order by protocol

--- src/com/rogermiranda1000/watchwolf/server/ServerConnector.java
+++ src/com/rogermiranda1000/watchwolf/server/ServerConnector.java
@@ -70,47 +70,6 @@ public class ServerConnector implements Runnable, ServerStartNotifier {
         }
     }
 
-    /* HELPER FUNCTIONS */
-    private static byte []toByteArray(ArrayList<Byte> bytes) {
-        byte []r = new byte[bytes.size()];
-        for (int x = 0; x < r.length; x++) r[x] = bytes.get(x);
-        return r;
-    }
-    private static void addRaw(ArrayList<Byte> out, Object []file) {
-        for (Byte b : (Byte[])file) out.add((byte)b);
-    }
-
-    private static void addArray(ArrayList<Byte> out, Object[] array, ArrayAdder arrayAdder) {
-        int size = array.length;
-        out.add((byte)((size>>8)&0xFF)); // MSB
-        out.add((byte)(size&0xFF));      // LSB
-
-        if (size > 0) arrayAdder.addToArray(out, array);
-    }
-
-    private static void addString(ArrayList<Byte> out, String str) {
-        Byte []arr = new Byte[str.length()];
-        for (int n = 0; n < arr.length; n++) arr[n] = (byte)str.charAt(n);
-        ServerConnector.addArray(out, arr, ServerConnector::addRaw);
-    }
-
-    private static String readString(DataInputStream dis) throws IOException {
-        // TODO check if EOF
-        // size
-        short size = (short) (dis.readByte() << 8); // MSB
-        size |= (short) dis.readByte(); // LSB
-
-        // characters
-        StringBuilder sb = new StringBuilder();
-        for (int n = 0; n < size; n++) sb.append((char)dis.readByte());
-        return sb.toString();
-    }
-
-    private static short readShort(DataInputStream dis) throws IOException {
-        short msb = (short)((short)dis.readByte() << 8);
-        return (short)(msb | dis.readByte()); // LSB
-    }
-
     /* server socket */
 
     @Override
@@ -172,19 +131,19 @@ public class ServerConnector implements Runnable, ServerStartNotifier {
         String nick;
         Position position;
         Block block;
-        short operation = SocketHelper.readShort(dis);
+        int operation = SocketHelper.readShort(dis);
         switch (operation) {
             case 0x0001:
                 this.executor.run(() -> this.serverPetition.stopServer(null));
                 break;
 
             case 0x0003:
-                nick = ServerConnector.readString(dis);
+                nick = SocketHelper.readString(dis);
                 this.executor.run(() -> this.serverPetition.whitelistPlayer(nick));
                 break;
 
             case 0x0004:
-                nick = ServerConnector.readString(dis);
+                nick = SocketHelper.readString(dis);
                 this.executor.run(() -> this.serverPetition.opPlayer(nick));
                 break;
 
@@ -203,8 +162,7 @@ public class ServerConnector implements Runnable, ServerStartNotifier {
                     // get block response header
                     msg.add((byte) 0b0001_1_001);
                     msg.add((byte) 0b00000000);
-                    msg.add((byte) 0x00);
-                    msg.add((byte) 0x06);
+                    msg.add((short) 0x0006);
 
                     msg.add(b);
 
@@ -221,17 +179,15 @@ public class ServerConnector implements Runnable, ServerStartNotifier {
 
     @Override
     public void onServerStart() throws IOException {
-        ArrayList<Byte> message = new ArrayList<>();
+        Message message = new Message(this.replySocket);
 
         // op player header
         message.add((byte) 0b0001_1_001);
         message.add((byte) 0b00000000);
-        message.add((byte) 0x00);
-        message.add((byte) 0x02);
+        message.add((short) 0x0002);
 
-        ServerConnector.addString(message, replyKey);
+        message.add(replyKey);
 
-        DataOutputStream dos = new DataOutputStream(this.replySocket.getOutputStream());
-        dos.write(ServerConnector.toByteArray(message), 0, message.size());
+        message.send();
     }
 }
