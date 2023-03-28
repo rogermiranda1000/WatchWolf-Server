52a2f00631041790a71ed560c311dd5f418ae01d
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-03-28 17:22:34 +0200

    Feature: memory optimized Feature: stop on timeout

--- src/main/java/dev/watchwolf/server/LogAppender.java
+++ src/main/java/dev/watchwolf/server/LogAppender.java
@@ -12,10 +12,10 @@ import java.util.ArrayList;
  */
 public class LogAppender extends AbstractAppender implements CommandRunner {
     private static final int WAIT_FOR_MESSAGE_TIMEOUT = 4000;
-    private static final int WAIT_BETWEEN_MESSAGES_TIMEOUT = 400;
+    private static final int WAIT_BETWEEN_MESSAGES_TIMEOUT = 40;
 
     private final JavaPlugin plugin;
-    private final ArrayList<String> logs = new ArrayList<>();
+    private ArrayList<String> logs = null;
 
     public LogAppender(JavaPlugin plugin) {
         // do your calculations here before starting to capture
@@ -31,12 +31,14 @@ public class LogAppender extends AbstractAppender implements CommandRunner {
 
         String msg = log.getMessage().getFormattedMessage();
         synchronized (this) {
-            this.logs.add(msg);
+            if (this.logs == null) return;
+            this.logs.add(msg); // TODO some WatchWolf msg are being appended as command return
         }
     }
 
     /**
-     * Important: this needs to be runned from a sepparate thread
+     * Important: this needs to be run from a separate thread
+     * Code based on the WatchWolf Client MineflayerClient's `send_command`
      * @param cmd Command to run
      * @return Response to the command
      */
@@ -44,19 +46,35 @@ public class LogAppender extends AbstractAppender implements CommandRunner {
     public String runCommand(String cmd) {
         // clear the previous logs
         synchronized (this) {
-            this.logs.clear(); // TODO send a signal to start capturing now
+            this.logs = new ArrayList<>();
         }
 
         // run the command
         Bukkit.dispatchCommand(this.plugin.getServer().getConsoleSender(), cmd);
 
         // get the command log
-        try {
-            Thread.sleep(400); // TODO implement as did in WatchWolf Client
-        } catch (InterruptedException e) {}
-        synchronized (this) {
-            System.out.println(this.logs.size());
-            return String.join("\n", this.logs);
+        long startTime = System.currentTimeMillis();
+        int previousLength = 0;
+        while ((System.currentTimeMillis() - startTime) < LogAppender.WAIT_FOR_MESSAGE_TIMEOUT) {
+            int length;
+            synchronized (this) {
+                length = this.logs.size();
+            }
+
+            if (length > 0 && previousLength == length) break; // timeout between messagess
+            previousLength = length;
+            try {
+                Thread.sleep(WAIT_BETWEEN_MESSAGES_TIMEOUT);
+            } catch (InterruptedException e) {}
         }
+
+        // it should be all done; now get the response
+        String response;
+        synchronized (this) {
+            response = String.join("\n", this.logs);
+            this.logs = null; // stop getting data
+        }
+
+        return response;
     }
 }
